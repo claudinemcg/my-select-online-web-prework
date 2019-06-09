c701a9985b4973a9067c41814f9ddf9ca5799ab8
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-09 08:34:13 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
 def my_select(collection)
   i = 0 
   new_arr = []
-  if true
+  
     while i < collection.length
       new_arr << (yield array[i])
     end
-  end
+  
     new_arr.compact
   
 end
