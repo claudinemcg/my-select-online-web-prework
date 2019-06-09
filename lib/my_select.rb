3021e69d330cd640734052cc9c924618dc529248
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-09 08:36:02 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -7,5 +7,17 @@ def my_select(collection)
     end
   
     new_arr.compact
-  
+
 end
+
+def my_select(collection) 
+  empty_arr = [] 
+  
+  i=0 
+  while i < collection.length
+  empty_arr.push yield(collection[i]) 
+  i += 1 
+end 
+
+  empty_arr.compact 
+end 
