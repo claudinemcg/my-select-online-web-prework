7ad859677245c9172f89013c9a25ba0a1c631bcf
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-09 08:37:17 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,25 +1,13 @@
-=begin
+
 def my_select(collection)
   i = 0 
   new_arr = []
   
     while i < collection.length
-      new_arr << (yield array[i])
+      new_arr << (yield collection[i])
+      i += 1
     end
   
     new_arr.compact
 
 end
-=end 
-
-def my_select(collection) 
-  empty_arr = [] 
-  
-  i=0 
-  while i < collection.length
-  empty_arr.push yield(collection[i]) 
-  i += 1 
-end 
-
-  empty_arr.compact 
-end 
